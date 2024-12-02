df039559ab03d8ece22fceef7fb553e57a8a78b8
soeun823 <bsoeun866@gmail.com>
2024-12-02 19:10:15 +0900

    refactor :: tab 2칸으로 바꿈
    tab 설정을 2칸으로 바꾸었습니다

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,9 @@
 import * as _ from './style';
 function Header(){
-    return(
-        <_.HeaderDiv>
-            <_.HeaderTitle>그리너리</_.HeaderTitle>
-        </_.HeaderDiv>
-    );
+  return(
+    <_.HeaderDiv>
+      <_.HeaderTitle>그리너리</_.HeaderTitle>
+    </_.HeaderDiv>
+  );
 }
 export default Header;
---
+++ src/pages/Start/indes.jsx
@@ -0,0 +1,19 @@
+import Header from "../../components/Header";
+
+export default function Start(){
+	return (
+		<div>
+			<Header/>
+			<div>
+				<div>
+					<h4>함께 걸어가는 작은 푸르름</h4>
+					<h1>그리너리</h1>
+				</div>
+				<button>
+					<p>로그인</p>
+					<img src="" alt="" />
+				</button>
+			</div>
+		</div>
+	);
+}
