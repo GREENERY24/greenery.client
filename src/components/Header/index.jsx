dcc5066ca5f68ee94c362cb28e67c5e97545a348
soeun823 <bsoeun866@gmail.com>
2024-12-02 17:19:36 +0900

    add :: 헤더 추가
    헤더를 추가합니다.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,9 @@
+import * as _ from './style';
 function Header(){
     return(
-        <h3>그리너리</h3>
+        <_.HeaderDiv>
+            <_.HeaderTitle>그리너리</_.HeaderTitle>
+        </_.HeaderDiv>
     );
 }
 export default Header;
---
+++ src/components/Header/style.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const HeaderDiv = styled.div`
+    width: 100%;
+    display: flex;
+    height: 3rem;
+    padding: 0.3rem 8rem;
+    align-items: center;
+    border-bottom: 2px solid #E4E4E4;
+`;
+export const HeaderTitle = styled.h3`
+    color: #A2DC7F;
+    font-family: ChangwonDangamAsac;
+    font-size: 1.75rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+`;
