929e9835e47ca56423593476dd901ad88d148d3c
soeun823 <bsoeun866@gmail.com>
2024-12-02 19:59:42 +0900

    docs :: 라우터 설정
    화면 간의 이동이 용이해지게 작성하였습니다

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'; // 최신 패키지 확인
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'; // 최신 패키지 확인
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        {/* <ReactQueryDevtools initialIsOpen={true} /> */}
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
