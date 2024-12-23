0dc6bb5b631a76f0889f4828c0f6967d7fe22435
soeun823 <bsoeun866@gmail.com>
2024-12-23 13:55:15 +0900

    fix :: 사진 불러오기
    사진불러올때 경로를 상대경로로 하여 문제를 해결하였습니다.

--- src/pages/MainView/index.jsx
+++ src/pages/MainView/index.jsx
@@ -1,6 +1,9 @@
 import { useNavigate } from 'react-router-dom';
 
 import Header from "../../components/Header";
+import Pot from '../../assets/Pot.svg';
+//날씨 사진
+import Sun from '../../assets/weather/날씨=낮_맑음.svg';
 
 // import * as _ from './style';
 export default function MainView(){
@@ -14,8 +17,8 @@ export default function MainView(){
 			/>
 			<div>
 				<div>
-					<img src="/" alt="" />
-					<img src="/" alt="" />
+					<img src={Sun} alt="" />
+					<img src={Pot} alt="" />
 				</div>
 				<div>
 					<div>온/습도</div>
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
   plugins: [react(), svgr()],
+  resolve: {
+    alias: {
+      '@': '/src', // '@'를 src 디렉토리로 매핑
+    },
+  },
 });
